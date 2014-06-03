c21b533637c3c90c0f6473d279fd2daa464f51e5
Marek <marecica2@gmail.com>
2014-06-03 11:11:58 +0200

    added support for amazon aws database + fixes in businessLogic.xml ( removed Id from db query )

--- src/main/resources/account.sql
+++ src/main/resources/account.sql
@@ -1,27 +1,3 @@
--- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: mule
--- ------------------------------------------------------
--- Server version	5.5.35-2
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `Account`
---
-
-DROP TABLE IF EXISTS `Account`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Account` (
   `Id` varchar(255) NOT NULL,
   `AccountNumber` varchar(255) DEFAULT NULL,
@@ -52,16 +28,5 @@ CREATE TABLE `Account` (
   `TickerSymbol` varchar(255) DEFAULT NULL,
   `Type` varchar(255) DEFAULT NULL,
   `Website` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-05-02 10:38:19
+  PRIMARY KEY (`id`)
+)
--- src/test/java/org/mule/templates/integration/BusinessLogicTest.java
+++ src/test/java/org/mule/templates/integration/BusinessLogicTest.java
@@ -5,6 +5,7 @@ import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +15,7 @@ import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mule.MessageExchangePattern;
 import org.mule.api.MuleEvent;
@@ -22,6 +24,7 @@ import org.mule.api.config.MuleProperties;
 import org.mule.api.lifecycle.InitialisationException;
 import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.templates.db.MySQLDbCreator;
 import org.mule.transport.NullPayload;
 import org.mule.util.UUID;
 
@@ -50,6 +53,11 @@ public class BusinessLogicTest extends FunctionalTestCase {
 	private static final String MAPPINGS_FOLDER_PATH = "./mappings";
 	private static final String TEST_FLOWS_FOLDER_PATH = "./src/test/resources/flows/";
 	private static final String MULE_DEPLOY_PROPERTIES_PATH = "./src/main/app/mule-deploy.properties";
+	
+	private static final String PATH_TO_TEST_PROPERTIES = "./src/test/resources/mule.test.properties";
+	private static final String PATH_TO_SQL_SCRIPT = "src/main/resources/account.sql";
+	private static final String DATABASE_NAME = "DB2SFDCAccountMigration" + new Long(new Date().getTime()).toString();
+	private static final MySQLDbCreator DBCREATOR = new MySQLDbCreator(DATABASE_NAME, PATH_TO_SQL_SCRIPT, PATH_TO_TEST_PROPERTIES);
 
 	protected static final int TIMEOUT_SEC = 120;
 	protected static final String TEMPLATE_NAME = "account-migration";
@@ -58,6 +66,11 @@ public class BusinessLogicTest extends FunctionalTestCase {
 	private List<Map<String, Object>> createdAccountsInDatabase = new ArrayList<Map<String, Object>>();
 	private BatchTestHelper helper;
 
+	@BeforeClass
+	public static void init() {
+		System.setProperty("db.jdbcUrl", DBCREATOR.getDatabaseUrlWithName());
+	}
+	
 	@Before
 	public void setUp() throws Exception {
 		helper = new BatchTestHelper(muleContext);
@@ -65,14 +78,15 @@ public class BusinessLogicTest extends FunctionalTestCase {
 		// Flow to retrieve accounts from target system after sync in Salesforce
 		retrieveAccountFromSalesforceFlow = getSubFlow("retrieveAccountFromSalesforceFlow");
 		retrieveAccountFromSalesforceFlow.initialise();
-	
+		
+		DBCREATOR.setUpDatabase();
 		createTestDataInSandBox();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		deleteTestAccountsFromDatabase();
 		deleteTestAccountsFromSalesforce();
+		DBCREATOR.tearDownDataBase();
 	}
 
 	@Test
@@ -85,13 +99,11 @@ public class BusinessLogicTest extends FunctionalTestCase {
 	
 		Map<String, Object> payload0 = invokeRetrieveFlow(retrieveAccountFromSalesforceFlow, createdAccountsInDatabase.get(0));
 		Assert.assertNotNull("The account 0 should have been sync but is null", payload0);
-		Assert.assertEquals("The account 0 should have been sync (Website)", createdAccountsInDatabase.get(0).get(KEY_WEBSITE), payload0.get(KEY_WEBSITE));
-		Assert.assertEquals("The account 0 should have been sync (Phone)", createdAccountsInDatabase.get(0).get(KEY_PHONE), payload0.get(KEY_PHONE));
+		Assert.assertEquals("The account 0 should have been sync (KEY_NAME)", createdAccountsInDatabase.get(0).get(KEY_NAME), payload0.get(KEY_NAME));
 
 		Map<String, Object>  payload1 = invokeRetrieveFlow(retrieveAccountFromSalesforceFlow, createdAccountsInDatabase.get(1));
 		Assert.assertNotNull("The account 1 should have been sync but is null", payload1);
-		Assert.assertEquals("The account 1 should have been sync (Website)", createdAccountsInDatabase.get(1).get(KEY_WEBSITE), payload1.get(KEY_WEBSITE));
-		Assert.assertEquals("The account 1 should have been sync (Phone)", createdAccountsInDatabase.get(1).get(KEY_PHONE), payload1.get(KEY_PHONE));
+		Assert.assertEquals("The account 1 should have been sync (Website)", createdAccountsInDatabase.get(1).get(KEY_NAME), payload1.get(KEY_NAME));
 		
 		Map<String, Object>  payload2 = invokeRetrieveFlow(retrieveAccountFromSalesforceFlow, createdAccountsInDatabase.get(2));
 		Assert.assertNull("The account 2 should have not been sync", payload2);
